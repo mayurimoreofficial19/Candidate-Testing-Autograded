3b929c8e570c89eb76ba99bb3820904737237210
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-05-12 19:19:17 -0500

    Graded assignment part-1 done!

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,11 +11,11 @@ let candidateAnswer;
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride", 	"true", 	"40", 	"Trajectory", 	3];
+let correctAnswers = "";
 let candidateAnswers;
 
 function askForName() {
-  console.log("hi2");
+  
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Enter candidate Name : ");
 }
@@ -47,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  console.log("hi1");
+  
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log("Candidate Name : " + candidateName);
