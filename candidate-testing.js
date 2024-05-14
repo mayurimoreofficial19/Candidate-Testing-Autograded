74c5799e1fc07a02c5311d7fe2d62539379747ee
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-05-13 20:55:12 -0500

    Candidate-Testing-Graded-Assignment-is-Done!

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,12 +7,12 @@ let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer;
+let candidateAnswer = '';
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
-let correctAnswers = "";
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
+let candidateAnswers = [];
 
 function askForName() {
   
@@ -22,26 +22,39 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  // candidateAnswer = input.question(question);
 
+  for(let i = 0; i < questions.length; i++)
+    {
+      candidateAnswer = input.question(questions[i]);
+
+      // console.log("***********************************************");
+      
+       console.log("Candidate Answer : " + candidateAnswers.push(candidateAnswer));
+      // console.log("CandidateAnswer inside askQuestion(): " + candidateAnswers);
+      
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if((correctAnswer === candidateAnswer) && correctAnswer.toUpperCase() === candidateAnswer.toUpperCase())
-    {
-      console.log("Congratulations! Your answer is right");
-    }
-    else
-    {
-      console.log("Candidate Answer : " + candidateAnswer);
-      console.log("Correct Answer : " + correctAnswer);
-    }
+  
+    // console.log("***********************************************");
+    // console.log("CandidateAnswer inside gradeQuiz():" + candidateAnswers);
+    // console.log("***********************************************");
 
-
+      for(let j = 0; j < correctAnswers.length; j++)
+        {
+          if((!correctAnswers.includes(candidateAnswers)))
+            {
+              console.log(`${questions[j]}`);
+              console.log(`Candidate Answer :  ${candidateAnswers[j]}`);
+              console.log(`Correct Answer :  ${correctAnswers[j]}`);
+            }
+        }
+ 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
 
   return grade;
 }
@@ -50,12 +63,16 @@ function runProgram() {
   
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Candidate Name : " + candidateName);
+   //console.log("Candidate Name : " + candidateName);
+
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  console.log("***********************************************");
+  console.log("Candidate Name : " + candidateName);
+  gradeQuiz(candidateAnswers);
 }
 
 runProgram();
+
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
