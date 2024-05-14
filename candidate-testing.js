e09b1b61f099bf8d3f04a9bf52e6fb56f527198c
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-05-14 10:34:30 -0500

    Graded Assignment part-2 is done

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
@@ -11,7 +11,7 @@ let candidateAnswer = '';
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName() {
@@ -63,13 +63,15 @@ function runProgram() {
   
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   //console.log("Candidate Name : " + candidateName);
+  console.log("Candidate Name : " + candidateName);
 
   askQuestion();
   console.log("***********************************************");
   console.log("Candidate Name : " + candidateName);
   gradeQuiz(this.candidateAnswers);
 }
+
+//runProgram();
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
